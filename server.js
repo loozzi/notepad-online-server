5cba6d928ad60eb1545fd795b10112b651bc72cc
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2022-09-16 10:21:23 +0700

    enable cors to my ip

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = 3000;
 
 // enable cors
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "192.168.1.2");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
