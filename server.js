a4ea319674c0cf3ed00b44f12f7587e79e98dcbf
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2022-09-16 10:24:02 +0700

    enable cors to my ip

--- server.js
+++ server.js
@@ -10,11 +10,16 @@ const app = express();
 const PORT = 3000;
 
 // enable cors
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "192.168.1.2");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+var allowlist = ['http://192.168.1.2:8080/', 'http://localhost:8080/']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 
 
 // setting app
